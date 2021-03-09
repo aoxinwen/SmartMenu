a855e2b6154ba92446e3e422f1b638e4f478bde6
mark ao <1178739685@qq.com>
2021-03-09 15:42:26 +0800

    首次提交

---
+++ Test_case/__init__.py
@@ -0,0 +1,7 @@
+# @Time : 2020/10/15 19:45 
+
+# @Author : Axw
+
+# @File : __init__.py.py 
+
+# @Software: PyCharm
---
+++ main.py
@@ -0,0 +1,9 @@
+# coding = utf-8
+
+"""
+# @Time : 2020/10/15 19:49 
+# @Author : Axw
+# @File : main.py 
+# @Software: PyCharm
+"""
+
